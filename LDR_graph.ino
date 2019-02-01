377e9bb60aa8907ea9ce8709936e9ca438d59e88
Alexander Zibin <37593964+AlexZibin@users.noreply.github.com>
2019-02-01 11:26:58 +0300

    Create LDR_graph.ino

---
+++ LDR_graph.ino
@@ -0,0 +1,13 @@
+#include
+
+// Collecting data each TIME_STEP milliseconds:
+#define TIME_STEP (1)
+// How much measurements we will collect:
+#define NUM_POINTS (250)
+
+void setup () {
+}
+
+void loop () {
+    // Loop is empty
+}
